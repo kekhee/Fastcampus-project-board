8ea5f7cdd3023ce1abc8c16cbf900757bfa62621
kimjian <kekhee@gmail.com>
2024-02-21 23:55:26 +0900

    Revert "#11-도메인 코드를 엔티티로 변환"
    This reverts commit 2395af643ed93245ca0c56160e5c7bd53382594c.

--- src/main/java/com/fastcampus/projectboard/repository/ArticleCommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fastcampus.projectboard.repository;
-
-import com.fastcampus.projectboard.domain.ArticleComment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
-}
